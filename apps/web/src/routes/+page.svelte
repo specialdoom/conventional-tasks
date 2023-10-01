5257f14427b61e6973d52a91d90c8100b5b91541
specialdoom <bledea.bogdan97@gmail.com>
2023-10-01 09:03:38 +0300

    chore: rename `ui` to `web`

---
+++ apps/web/src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import {parseInline} from "marked";
+
+  let todos: string[] = [];
+
+  function onInputChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    const value = target.value;
+
+    todos = [value, ...todos];
+  }
+</script>
+
+<div>
+  {#each todos as todo}
+    <!-- eslint-disable -->
+    <p>{@html parseInline(todo)}</p>
+  {/each}
+</div>
+
+<input
+  type="text"
+  on:change={onInputChange}
+/><button>+</button>
