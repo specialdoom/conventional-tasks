c19f016a50c300cede8eea8751c55160fb46174a
specialdoom <bledea.bogdan97@gmail.com>
2023-10-28 16:13:40 +0300

    feat: add `Logo` component

---
+++ apps/web/src/lib/client/components/Logo.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+</script>
+
+<svg
+  width="20"
+  height="20"
+  viewBox="0 0 80 80"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <rect
+    x="10"
+    y="10"
+    width="68"
+    height="68"
+    fill="black"
+    stroke="black"
+    stroke-width="4"
+    stroke-linejoin="round"
+  />
+  <rect
+    x="2"
+    y="2"
+    width="68"
+    height="68"
+    fill="#00FF75"
+    stroke="black"
+    stroke-width="4"
+    stroke-linejoin="round"
+  />
+  <path
+    d="M58 24L34 48L18 32"
+    stroke="black"
+    stroke-width="8"
+  />
+</svg>
+conventional-tasks
